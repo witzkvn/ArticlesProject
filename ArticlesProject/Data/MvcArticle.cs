4e937c79f63c501e8cb167cee07beee8a2146476
witzkvn <witz.kevin05@gmail.com>
2021-10-30 18:30:04 +0200

    Ajoutez des fichiers projet.

---
+++ ArticlesProject/Data/MvcArticle.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ArticlesProject.Models;
+
+namespace ArticlesProject.Data
+{
+    public class MvcArticleContext : DbContext
+    {
+        public MvcArticleContext (DbContextOptions<MvcArticleContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Article { get; set; }
+    }
+}
